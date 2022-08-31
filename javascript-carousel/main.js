0f5a9e17c33f4b6a0e871002b069ef96edd47aef
Macy Hong <macyhong@gmail.com>
2022-08-31 04:24:15 +0000

    Carousel loops from last slide to first now

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -6,6 +6,9 @@ var $dot = document.querySelectorAll('.fa-circle');
 var $dots = document.querySelector('.buttons');
 var timer = null;
 
+clearInterval(timer);
+timer = setInterval(next, 3000);
+
 $rightArr.addEventListener('click', next);
 function next() {
   for (let i = 0; i < $img.length; i++) {
@@ -14,13 +17,19 @@ function next() {
       $img[i].nextElementSibling.classList = 'show';
       $dot[i].classList.replace('fa-solid', 'fa-regular');
       $dot[i].nextElementSibling.classList.replace('fa-regular', 'fa-solid');
+      clearInterval(timer);
+      timer = setInterval(next, 3000);
       break;
+    } if ($img[i].className === 'show' && i === $img.length - 1) {
+      $img[0].className = 'show';
+      $img[i].className = 'hidden';
+      $dot[0].classList.replace('fa-regular', 'fa-solid');
+      $dot[i].classList.replace('fa-solid', 'fa-regular');
+      clearInterval(timer);
+      timer = setInterval(next, 3000);
     }
   }
 }
-
-clearInterval(timer);
-timer = setInterval(next, 3000);
 
 $leftArr.addEventListener('click', prev);
 function prev() {
@@ -30,11 +39,20 @@ function prev() {
       $img[i].previousElementSibling.className = 'show';
       $dot[i].classList.replace('fa-solid', 'fa-regular');
       $dot[i].previousElementSibling.classList.replace('fa-regular', 'fa-solid');
+      clearInterval(timer);
+      timer = setInterval(next, 3000);
+      break;
+    }
+    if ($img[i].className === 'show' && i === 0) {
+      $img[0].className = 'hidden';
+      $img[$img.length - 1].className = 'show';
+      $dot[0].classList.replace('fa-solid', 'fa-regular');
+      $dot[$img.length - 1].classList.replace('fa-regular', 'fa-solid');
+      clearInterval(timer);
+      timer = setInterval(next, 3000);
       break;
     }
   }
-  clearInterval(timer);
-  timer = setInterval(next, 3000);
 }
 
 $dots.addEventListener('click', update);
@@ -43,8 +61,7 @@ function update(event) {
     if ($dot[i].classList.contains('fa-solid')) {
       $dot[i].classList.replace('fa-solid', 'fa-regular');
       $img[i].className = 'hidden';
-    }
-    if (event.target.classList.contains('fa-regular')) {
+    } if (event.target.classList.contains('fa-regular')) {
       event.target.classList.replace('fa-regular', 'fa-solid');
       for (let j = 0; j < $dot.length; j++) {
         if ($dot[j].classList.contains('fa-solid')) {
