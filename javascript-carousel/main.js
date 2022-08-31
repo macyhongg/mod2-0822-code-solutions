8f389c2589905752507255c4111b3f9b0147df94
Macy Hong <macyhong@gmail.com>
2022-08-31 03:37:11 +0000

    Completed javascript-carousel

---
+++ javascript-carousel/main.js
@@ -0,0 +1,58 @@
+// DATA
+var $leftArr = document.querySelector('.fa-angle-left');
+var $rightArr = document.querySelector('.fa-angle-right');
+var $img = document.querySelectorAll('img');
+var $dot = document.querySelectorAll('.fa-circle');
+var $dots = document.querySelector('.buttons');
+var timer = null;
+
+$rightArr.addEventListener('click', next);
+function next() {
+  for (let i = 0; i < $img.length; i++) {
+    if ($img[i].className === 'show' && i !== $img.length - 1) {
+      $img[i].className = 'hidden';
+      $img[i].nextElementSibling.classList = 'show';
+      $dot[i].classList.replace('fa-solid', 'fa-regular');
+      $dot[i].nextElementSibling.classList.replace('fa-regular', 'fa-solid');
+      break;
+    }
+  }
+}
+
+clearInterval(timer);
+timer = setInterval(next, 3000);
+
+$leftArr.addEventListener('click', prev);
+function prev() {
+  for (let i = 0; i < $img.length; i++) {
+    if ($img[i].className === 'show' && i !== 0) {
+      $img[i].className = 'hidden';
+      $img[i].previousElementSibling.className = 'show';
+      $dot[i].classList.replace('fa-solid', 'fa-regular');
+      $dot[i].previousElementSibling.classList.replace('fa-regular', 'fa-solid');
+      break;
+    }
+  }
+  clearInterval(timer);
+  timer = setInterval(next, 3000);
+}
+
+$dots.addEventListener('click', update);
+function update(event) {
+  for (let i = 0; i < $dot.length; i++) {
+    if ($dot[i].classList.contains('fa-solid')) {
+      $dot[i].classList.replace('fa-solid', 'fa-regular');
+      $img[i].className = 'hidden';
+    }
+    if (event.target.classList.contains('fa-regular')) {
+      event.target.classList.replace('fa-regular', 'fa-solid');
+      for (let j = 0; j < $dot.length; j++) {
+        if ($dot[j].classList.contains('fa-solid')) {
+          $img[j].className = 'show';
+        }
+      }
+    }
+  }
+  clearInterval(timer);
+  timer = setInterval(next, 3000);
+}
