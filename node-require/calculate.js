cc7cacddb725d80ad92a4143b842653460ce3c61
Macy Hong <macyhong@gmail.com>
2022-10-05 01:56:32 +0000

    Accounts for all numbers

--- node-require/calculate.js
+++ node-require/calculate.js
@@ -4,11 +4,13 @@ const multiply = require('./multiply');
 const divide = require('./divide');
 
 if (process.argv[3] === 'plus') {
-  console.log('result: ', add(parseInt(process.argv[2]), parseInt(process.argv[4])));
+  console.log('result: ', add(Number(process.argv[2]), Number(process.argv[4])));
 } else if (process.argv[3] === 'minus') {
-  console.log('result: ', subtract(parseInt(process.argv[2]), parseInt(process.argv[4])));
+  console.log('result: ', subtract(Number(process.argv[2]), Number(process.argv[4])));
 } else if (process.argv[3] === 'times') {
-  console.log('result: ', multiply(parseInt(process.argv[2]), parseInt(process.argv[4])));
+  console.log('result: ', multiply(Number(process.argv[2]), Number(process.argv[4])));
 } else if (process.argv[3] === 'over') {
-  console.log('result: ', divide(parseInt(process.argv[2]), parseInt(process.argv[4])));
+  console.log('result: ', divide(Number(process.argv[2]), Number(process.argv[4])));
+} else {
+  console.log('invalid operation');
 }
